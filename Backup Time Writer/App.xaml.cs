8cd4ec4f21444d2cbd34a2be6802761de02d8d07
Victor Carri <victorcarri@gmail.com>
2022-08-05 15:59:55 -0400

    Removed unnecessary startup handler.

--- Backup Time Writer/App.xaml.cs
+++ Backup Time Writer/App.xaml.cs
@@ -8,15 +8,5 @@ namespace Backup_Time_Writer
     /// </summary>
     public partial class App : Application
     {
-        /**
-        **/
-        private void Application_Startup(object sender, StartupEventArgs e)
-        {
-            if (!Directory.Exists(ConfigModel.BackupDrive)) // No backup drive connected
-            {
-                MessageBox.Show("The backup drive " + ConfigModel.BackupDrive + " isn't connected to the computer.", "Backup Drive Not Found", MessageBoxButton.OK);
-                this.Shutdown((int)ExitCode.Code.NO_BACKUP_DRIVE);
-            }
-        }
     }
 }
